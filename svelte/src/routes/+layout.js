68f987d6b95f5f552f21f1d52a38e1efd48f20ab
Aizhee <Aizhee@users.noreply.github.com>
2025-07-22 03:38:17 +0000

    [svelte] [ml-models] [svelte] Delete svelte/.github/workflows directory

---
+++ svelte/src/routes/+layout.js
@@ -0,0 +1,2 @@
+export const ssr = false;
+import { animate, stagger } from 'animejs';
---
+++ svelte/src/routes/+layout.svelte
@@ -0,0 +1,67 @@
+<script>
+	import Header from './Header.svelte';
+	import '../app.css';
+	import Navigation from './Navigation.svelte';
+	import { page } from '$app/stores';
+	import { derived } from 'svelte/store';
+	import bg from '$lib/images/background.webm';
+	const path = derived(page, $page => $page.url.pathname);
+	let videoEl;
+
+	$effect(() => {
+		setTimeout(() => {
+			if (videoEl) {
+			$path === '/chatbot' ? videoEl.pause() : videoEl.play();
+			}
+		}, 1000);
+	}); 
+
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<Header />
+<video bind:this={videoEl} autoplay loop muted playsinline class="background" class:blur={$path === '/chatbot'}>
+  <source src={bg} type="video/webm" />
+  Your browser does not support the video tag.
+</video>
+
+	<div class="content">
+		<Navigation />
+		<main>
+			{@render children()}
+		</main>
+	</div>
+	
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+		box-sizing: border-box;
+		font-family: 'Inter', sans-serif;
+	}
+	.background {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: auto;
+		display:block;
+		z-index: -1;
+		pointer-events: none;
+		filter: blur(0px);
+		transition: filter 2s ease;
+	}
+
+	.blur{
+		filter: blur(5px);
+	}
+	.content {
+		display: flex;
+		flex: 1;
+	}	
+
+</style>
